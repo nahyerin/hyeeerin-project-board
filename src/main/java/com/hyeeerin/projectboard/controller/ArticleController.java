1c03ac5ffe9a669c7da7084c8dc4098666c7ae5b
nahyerin <with8675@gmail.com>
2024-04-05 14:21:51 +0900

    #20 - 게시글 페이지네이션 구현
    `이전 글`, `다음 글`만 구현하므로 게시판에 비해 단순하지만
    첫 글, 마지막 글은 버튼 비활성화 및 가짜 링크 처리해야 해서
    타임리프 문법이 복잡해짐
    
    마지막 글을 판단하기 위해 총 글 개수가 필요해졌고
    이를 위해 count 쿼리 사용, 서비스 로직 추가

--- src/main/java/com/hyeeerin/projectboard/controller/ArticleController.java
+++ src/main/java/com/hyeeerin/projectboard/controller/ArticleController.java
@@ -1,40 +1,52 @@
 package com.hyeeerin.projectboard.controller;
 
-import com.hyeeerin.projectboard.response.ArticleResponse;
+
+import com.hyeeerin.projectboard.domain.type.SearchType;
+import com.hyeeerin.projectboard.dto.response.ArticleResponse;
+import com.hyeeerin.projectboard.dto.response.ArticleWithCommentsResponse;
 import com.hyeeerin.projectboard.service.ArticleService;
 import com.hyeeerin.projectboard.service.PaginationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
-
     private final ArticleService articleService;
     private final PaginationService paginationService;
     @GetMapping
-    public String articles(ModelMap map) {
-        map.addAttribute("articles", List.of());
-
-        return "articles/index";
-    }
-
-    @GetMapping("/{articleId}")
-    public String article(@PathVariable Long articleId, ModelMap map) {
+    public String articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map
+    ) {
         Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
         List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
-
         map.addAttribute("articles", articles);
         map.addAttribute("paginationBarNumbers", barNumbers);
+        return "articles/index";
+    }
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+
+        map.addAttribute("article", article);
+        map.addAttribute("articleComments", article.articleCommentsResponse());
+        map.addAttribute("totalCount", articleService.getArticleCount());
 
         return "articles/detail";
     }
-
-
 }
